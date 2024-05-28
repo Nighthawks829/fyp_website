73f71a2b24def43a75a435483095b60724a6a2af
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-05-28 10:19:48 +0800

    Delete Sensor Modal

--- src/pages/sensor/SensorPage.js
+++ src/pages/sensor/SensorPage.js
@@ -6,72 +6,113 @@ import { Link } from "react-router-dom";
 export default function SensorPage() {
   const navigate = useNavigate();
   return (
-    <div className="p-xl-5 p-3">
-      <div className="text-end mb-4">
-        <button
-          className="add-btn btn-primary fw-bold shadow px-3 py-1"
-          onClick={() => navigate("/addSensor")}
-        >
-          + Sensor
-        </button>
-      </div>
-      <table className="table">
-        <thead>
-          <th className="text-center" scope="col">
-            Name
-          </th>
-          <th className="text-center" scope="col">
-            Type
-          </th>
-          <th className="text-center" scope="col">
-            Pin
-          </th>
-          <th className="text-center" scope="col">
-            Board
-          </th>
-          <th className="text-center" scope="col">
-            Topc
-          </th>
-          <th className="text-center" scope="col"></th>
-        </thead>
-        <tbody className="table-group-divider">
-          <tr>
-            <td className="text-center">
-              <Link to='/viewSensor/1'>Sensor 1</Link>
-            </td>
-            <td className="text-center">Anaglog Input</td>
-            <td className="text-center">2</td>
-            <td className="text-center">Board1</td>
-            <td className="text-center">esp32/board1/output</td>
-            <td className="text-center py-2 action">
-              <div className="dropdown">
-                <IoIosMore
-                  size={25}
-                  className="dropdown-toggle"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                />
-                <ul className="dropdown-menu py-3">
-                  <li className="ps-1 pe-2 mb-2">
-                    <Link className="dropdown-item text-dark py-2 m-0" to='/editSensor/1'>
-                      Edit
-                    </Link>
-                  </li>
-                  <li className="ps-1 pe-2">
-                    <a
-                      className="dropdown-item text-danger py-2 m-0 mb-1"
-                      href="/"
-                    >
-                      Delete
-                    </a>
-                  </li>
-                </ul>
+    <>
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="deleteSensor"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby="deleteSensorLabel1"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body p-5 shadow">
+              <h2 className="text-center mb-5">
+                Are you sure want to delete Sensor 1?
+              </h2>
+              <div className="d-flex align-items-center justify-content-evenly mt-5">
+                <button
+                  className="modal-cancel-button shadow"
+                  data-bs-dismiss="modal"
+                >
+                  Yes
+                </button>
+                <button
+                  className="modal-next-button shadow"
+                  data-bs-dismiss="modal"
+                >
+                  No
+                </button>
               </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-xl-5 p-3">
+        <div className="text-end mb-4">
+          <button
+            className="add-btn btn-primary fw-bold shadow px-3 py-1"
+            onClick={() => navigate("/addSensor")}
+          >
+            + Sensor
+          </button>
+        </div>
+        <table className="table">
+          <thead>
+            <th className="text-center" scope="col">
+              Name
+            </th>
+            <th className="text-center" scope="col">
+              Type
+            </th>
+            <th className="text-center" scope="col">
+              Pin
+            </th>
+            <th className="text-center" scope="col">
+              Board
+            </th>
+            <th className="text-center" scope="col">
+              Topc
+            </th>
+            <th className="text-center" scope="col"></th>
+          </thead>
+          <tbody className="table-group-divider">
+            <tr>
+              <td className="text-center">
+                <Link to="/viewSensor/1">Sensor 1</Link>
+              </td>
+              <td className="text-center">Anaglog Input</td>
+              <td className="text-center">2</td>
+              <td className="text-center">Board1</td>
+              <td className="text-center">esp32/board1/output</td>
+              <td className="text-center py-2 action">
+                <div className="dropdown">
+                  <IoIosMore
+                    size={25}
+                    className="dropdown-toggle"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  />
+                  <ul className="dropdown-menu py-3">
+                    <li className="ps-1 pe-2 mb-2">
+                      <Link
+                        className="dropdown-item text-dark py-2 m-0"
+                        to="/editSensor/1"
+                      >
+                        Edit
+                      </Link>
+                    </li>
+                    <li className="ps-1 pe-2">
+                      <buttton
+                        className="dropdown-item text-danger py-2 m-0 mb-1"
+                        data-bs-toggle="modal"
+                        data-bs-target="#deleteSensor"
+                      >
+                        Delete
+                      </buttton>
+                    </li>
+                  </ul>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
--- src/pages/viewSensor/ViewSensorPage.js
+++ src/pages/viewSensor/ViewSensorPage.js
@@ -6,80 +6,125 @@ export default function ViewSensorPage() {
   //   const { id } = useParams();
   const navigate = useNavigate();
   return (
-    <div className="p-xl-5 p-3">
-      <div className="text-start">
-        <button
-          className="back-btn btn-primary fw-bold shadow px-4 py-1"
-          onClick={() => navigate(-1)}
-        >
-          Back
-        </button>
+    <>
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="deleteSensor"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby="deleteSensorLabel1"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body p-5 shadow">
+              <h2 className="text-center mb-5">
+                Are you sure want to delete Sensor 1?
+              </h2>
+              <div className="d-flex align-items-center justify-content-evenly mt-5">
+                <button
+                  className="modal-cancel-button shadow"
+                  data-bs-dismiss="modal"
+                >
+                  Yes
+                </button>
+                <button
+                  className="modal-next-button shadow"
+                  data-bs-dismiss="modal"
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="text-center">
-        <img
-          src={require("../../assets/led.jpeg")}
-          alt=""
-          className="board-img"
-        />
-        <div className="mt-4 col-12 text-center">
-          <div className="d-flex flex-wrap align-items-center justify-content-center">
-            <button className="px-3 py-1 edit-button shadow m-1">ON</button>
-            <button className="px-3 py-1 delete-button shadow m-1">OFF</button>
-          </div>
-          <h3 className="mt-4">
-            State: <span className="digital-value">ON</span>
-          </h3>
-        </div>
-        <div className="col-lg-8 col-12 mx-auto mt-5">
-          <div className="row">
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                Name: <span className="board-data ms-1">Board1</span>
-              </h5>
-            </div>
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                Pin: <span className="board-data ms-1">7</span>
-              </h5>
-            </div>
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                Type: <span className="board-data ms-1">Digital Output</span>
-              </h5>
-            </div>
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                Board: <span className="board-data ms-1">Board1</span>
-              </h5>
-            </div>
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                Topic:{" "}
-                <span className="board-data ms-1">
-                  esp32/board1/output/led1
-                </span>
-              </h5>
-            </div>
-            <div className="col-lg-6 col-12 mb-3">
-              <h5 className="fw-bold">
-                State: <span className="board-data ms-1">ON</span>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-5 col-12 text-center ">
-        <div className="d-flex flex-wrap align-items-center justify-content-center">
+      <div className="p-xl-5 p-3">
+        <div className="text-start">
           <button
-            className="px-3 py-1 edit-button shadow m-1"
-            onClick={() => navigate(`/editSensor/1`)}
+            className="back-btn btn-primary fw-bold shadow px-4 py-1"
+            onClick={() => navigate(-1)}
           >
-            Edit
+            Back
           </button>
-          <button className="px-3 py-1 delete-button shadow m-1">Delete</button>
+        </div>
+
+        <div className="text-center">
+          <img
+            src={require("../../assets/led.jpeg")}
+            alt=""
+            className="board-img"
+          />
+          <div className="mt-4 col-12 text-center">
+            <div className="d-flex flex-wrap align-items-center justify-content-center">
+              <button className="px-3 py-1 edit-button shadow m-1">ON</button>
+              <button className="px-3 py-1 delete-button shadow m-1">
+                OFF
+              </button>
+            </div>
+            <h3 className="mt-4">
+              State: <span className="digital-value">ON</span>
+            </h3>
+          </div>
+          <div className="col-lg-8 col-12 mx-auto mt-5">
+            <div className="row">
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  Name: <span className="board-data ms-1">Board1</span>
+                </h5>
+              </div>
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  Pin: <span className="board-data ms-1">7</span>
+                </h5>
+              </div>
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  Type: <span className="board-data ms-1">Digital Output</span>
+                </h5>
+              </div>
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  Board: <span className="board-data ms-1">Board1</span>
+                </h5>
+              </div>
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  Topic:{" "}
+                  <span className="board-data ms-1">
+                    esp32/board1/output/led1
+                  </span>
+                </h5>
+              </div>
+              <div className="col-lg-6 col-12 mb-3">
+                <h5 className="fw-bold">
+                  State: <span className="board-data ms-1">ON</span>
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-5 col-12 text-center ">
+          <div className="d-flex flex-wrap align-items-center justify-content-center">
+            <button
+              className="px-3 py-1 edit-button shadow m-1"
+              onClick={() => navigate(`/editSensor/1`)}
+            >
+              Edit
+            </button>
+            <button
+              className="px-3 py-1 delete-button shadow m-1"
+              data-bs-toggle="modal"
+              data-bs-target="#deleteSensor"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
