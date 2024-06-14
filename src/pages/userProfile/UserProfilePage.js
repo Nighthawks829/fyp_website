47b8eff1a2fd2c0daf74c41715982bcd4d239aee
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2024-06-14 14:54:42 +0800

    Modified userProfilePage to get the the user profile by redux

--- src/pages/userProfile/UserProfilePage.js
+++ src/pages/userProfile/UserProfilePage.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import defaultImage from "../../assets/profile.jpg";
+import { getUser } from "../../stores/user/userSlice";
 
 export default function UserProfilePage() {
   const navigate = useNavigate();
+  const { user } = useSelector((store) => store.auth);
+  const { name, email, role, image } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const UserImage = ({ image }) => {
+    const [imgSrc, setImgSrc] = useState("");
+
+    useEffect(() => {
+      try {
+        setImgSrc(require(`../../../public/uploads/${image}`));
+      } catch (error) {
+        setImgSrc(defaultImage);
+      }
+    }, [image]);
+
+    return <img src={imgSrc} alt="" className="user-img" />;
+  };
+
+  useEffect(() => {
+    dispatch(getUser(user.userId));
+  }, []);
 
   return (
     <div className="p-xl-5 p-3">
@@ -17,20 +41,21 @@ export default function UserProfilePage() {
         <div style={{ width: "106.2px" }}></div>
       </div>
       <div className="text-center">
-        <img
+        {/* <img
           src={require("../../assets/profile.jpg")}
           alt=""
           className="user-img"
-        />
+        /> */}
+        <UserImage image={image} />
         <div className="mt-5">
           <h5 className="mb-3 fw-bold">
-            Name: <span className="board-data">Nighthawks</span>
+            Name: <span className="board-data">{name}</span>
           </h5>
           <h5 className="mb-3 fw-bold">
-            Role: <span className="board-data">Admin</span>
+            Role: <span className="board-data">{role}</span>
           </h5>
           <h5 className="mb-3 fw-bold">
-            Email: <span className="board-data">nighthawks0230@gmail.com</span>
+            Email: <span className="board-data">{email}</span>
           </h5>
         </div>
         <div className="mt-5 col-12 text-center ">
